0c16d639b8c0c2e6bfb683c080c8945a781609d4
Vra Widia Putri <vrawidiaputri@gmail.com>
2023-07-12 21:13:20 +0700

    penambahan

--- app/src/main/java/com/example/nozel/ui/cari/CariFragment.kt
+++ app/src/main/java/com/example/nozel/ui/cari/CariFragment.kt
@@ -1,40 +1,68 @@
-package com.example.nozel.ui.cari
+package com.example.Nozel.ui.Cari
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
-import com.example.nozel.databinding.FragmentCariBinding
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.Nozel.adapter.adapterriwayat
 
 
-class CariFragment : Fragment() {
+import com.example.Nozel.app.ApiConfig.ApiConfig
+import com.example.Nozel.model.RiwayatModel
+import com.example.Nozel.model.ResponseModel
+import com.example.nozel.R
+import retrofit2.Call
+import retrofit2.Response
 
-    private var _binding: FragmentCariBinding? = null
+class CariFragment: Fragment() {
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
+    //
+    lateinit var rvRiwayat: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        val cariViewModel =
-            ViewModelProvider(this).get(CariViewModel::class.java)
+    ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_cari, container, false)
 
-        _binding = FragmentCariBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-
-
-        return root
+        init(view)
+        getRiwayat()
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    private var listRiwayat: ArrayList<RiwayatModel> = ArrayList()
+
+    fun getRiwayat(){
+        ApiConfig.instanceRetrofit.getRiwayat().enqueue(object :
+            retrofit2.Callback<ResponseModel> {
+
+            override fun onResponse(call: Call<ResponseModel>, response: Response<ResponseModel>) {
+                val res = response.body()!!
+                listRiwayat = res.riwayat
+                displayRiwayat()
+            }
+            override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
+                Toast.makeText(requireActivity(), "Error :"+t.message, Toast.LENGTH_LONG).show()
+            }
+        })
     }
+
+    fun displayRiwayat() {
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        rvRiwayat.adapter = adapterriwayat(requireActivity(), listRiwayat)
+        rvRiwayat.layoutManager = layoutManager
+    }
+
+    fun init(view: View) {
+        //rvBarang = view.findViewById(R.id.recyler_view)
+        rvRiwayat=view.findViewById(R.id.recyler_view)
+    }
+
 }
